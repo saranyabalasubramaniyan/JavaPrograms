41ef3c4d084438c76cb1916098a911d5a3beb5c1
saranyabalasubramaniyan <98370921+saranyabalasubramaniyan@users.noreply.github.com>
2024-10-30 00:21:21 -0500

    Add files via upload

---
+++ Print_DogImage/PrintingTheImageOfADog.java
@@ -0,0 +1,74 @@
+/*********************************************************************************
+ *                            Name : Saranya Balasubramaniyan
+ *							 
+ * This class has the solution for Assignment 1.b. The class prints the image of a dog
+ * from the website - https://asciiart.website/index.php?art=animals/dogs
+ *********************************************************************************/
+
+public class Assignment1b
+{
+	public static void main(String args[])
+	{
+		System.out.println("*************** Assignment 1.b - START *******************");
+		System.out.println("Name: Saranya Balasubramaniyan");
+	
+		
+		System.out.println("** Printing the Image of a Dog **");
+		
+		System.out.println("                                         do.");
+		System.out.println("                                        :NOX");
+		System.out.println("                                       ,NOM@:");
+		System.out.println("                                       :NNNN:");
+		System.out.println("                                       :XXXON");
+		System.out.println("                                       :XoXXX.");
+		System.out.println("                                       MM;ONO:");
+		System.out.println("  .oob..                              :MMO;MOM");
+		System.out.println(" dXOXYYNNb.                          ,NNMX:MXN");
+		System.out.println(" Mo\"'  '':Nbb                        dNMMN MNN:");
+		System.out.println(" Mo  'O;; ':Mb.                     ,MXMNM MNX:");
+		System.out.println(" @O :;XXMN..'X@b.                  ,NXOMXM MNX:");
+		System.out.println(" YX;;NMMMM@M;;OM@o.                dXOOMMN:MNX:");
+		System.out.println(" 'MOONM@@@MMN:':NONb.            ,dXONM@@MbMXX:");
+		System.out.println("  MOON@M@@MMMM;;:OOONb          ,MX'\"':ONMMMMX:");
+		System.out.println("  :NOOM@@MNNN@@X;\"\"XNN@Mb     .dP\"'   ,..OXM@N:");
+		System.out.println("   MOON@@MMNXXMMO  :M@@M...@o.oN\"\"\":OOOXNNXXOo:");
+		System.out.println("   :NOX@@@MNXXXMNo :MMMM@K\"`,:;NNM@@NXM@MNO;.'N.");
+		System.out.println("    NO:X@@MNXXX@@O:'X@@@@MOOOXMM@M@NXXN@M@NOO ''b");
+		System.out.println("    `MO.'NMNXXN@@N: 'XXM@NMMXXMM@M@XO\"'\"XM@X;.  :b");
+		System.out.println("     YNO;'\"NXXXX@M;;::\"XMNN:\"\"ON@@MO: ,;;.:Y@X: :OX.");
+		System.out.println("      Y@Mb;;XNMM@@@NO: ':O: 'OXN@@MO\" ONMMX:`XO; :X@.");
+		System.out.println("      '@XMX':OX@@MN:    ;O;  :OX@MO\" 'OMM@N; ':OO;N@N");
+		System.out.println("       YN;\":.:OXMX\"': ,:NNO;';XMMX:  ,;@@MNN.'.:O;:@X:");
+		System.out.println("       `@N;;XOOOXO;;:O;:@MOO;:O:\"\" ,oMP@@K\"YM.;NMO;`NM");
+		System.out.println("        `@@MN@MOX@@MNMN;@@MNXXOO: ,d@NbMMP'd@@OX@NO;.'bb.");
+		System.out.println("       .odMX@@XOOM@M@@XO@MMMMMMNNbN\"YNNNXoNMNMO\"OXXNO..\"\";o.");
+		System.out.println("     .ddMNOO@@XOOM@@XOONMMM@@MNXXMMo;.\"' .\":OXO ':.'\"'\"'  '\"\"o.");
+		System.out.println("    'N@@X;,M@MXOOM@OOON@MM@MXOO:\":ONMNXXOXX:OOO               \"\"ob.");
+		System.out.println("   ')@MP\"';@@XXOOMMOOM@MNNMOO\"\"   '\"OXM@MM: :OO.        :...';o;.;Xb.");
+		System.out.println("  .@@MX\" ;X@@XXOOM@OOXXOO:o:'      :OXMNO\"' ;OOO;.:     ,OXMOOXXXOOXMb");
+		System.out.println(" ,dMOo:  oO@@MNOON@N:::\"      .    ,;O:\"\"'  .dMXXO:    ,;OX@XO\"\":ON@M@");
+		System.out.println(":Y@MX:.  oO@M@NOXN@NO. ..: ,;;O;.       :.OX@@MOO;..   .OOMNMO.;XN@M@P");
+		System.out.println(",MP\"OO'  oO@M@O:ON@MO;;XO;:OXMNOO;.  ,.;.;OXXN@MNXO;.. oOX@NMMN@@@@@M:");
+		System.out.println("`' \"O:;;OON@@MN::XNMOOMXOOOM@@MMNXO:;XXNNMNXXXN@MNXOOOOOXNM@NM@@@M@MP");
+		System.out.println("   :XN@MMM@M@M:  :'OON@@XXNM@M@MXOOdN@@@MM@@@@MMNNXOOOXXNNN@@M@MMMM\"'");
+		System.out.println("   .oNM@MM@ONO'   :;ON@@MM@MMNNXXXM@@@@M@PY@@MMNNNNNNNNNNNM@M@M@@P'");
+		System.out.println("  ;O:OXM@MNOOO.   'OXOONM@MNNMMXON@MM@@b. 'Y@@@@@@@@@@@@@M@@MP\"'\"");
+		System.out.println(" ;O':OOXNXOOXX:   :;NMO:\":NMMMXOOX@MN@@@@b.:M@@@M@@@MMM@\"\"\"\"");
+		System.out.println(" :: ;\"OOOOOO@N;:  'ON@MO.'\":\"\"OOOO@@NNMN@@@. Y@@@MMM@@@@b");
+		System.out.println(" :;   ':O:oX@@O;;  ;O@@XO'   \"oOOOOXMMNMNNN@MN\"\"YMNMMM@@MMo.");
+		System.out.println(" :N:.   ''oOM@NMo.::OX@NOOo.  ;OOOXXNNNMMMNXNM@bd@MNNMMM@MM@bb");
+		System.out.println("  @;O .  ,OOO@@@MX;;ON@NOOO.. ' ':OXN@NNN@@@@@M@@@@MNXNMM@MMM@,");
+		System.out.println("  M@O;;  :O:OX@@M@NXXOM@NOO:;;:,;;ON@NNNMM'`\"@@M@@@@@MXNMMMMM@N");
+		System.out.println("  N@NOO;:oO;O:NMMM@M@OO@NOO;O;oOOXN@NNM@@'   `Y@NM@@@@MMNNMM@MM");
+		System.out.println("  ::@MOO;oO:::OXNM@@MXOM@OOOOOOXNMMNNNMNP      \"\"MNNM@@@MMMM@MP");
+		System.out.println("    @@@XOOO':::OOXXMNOO@@OOOOXNN@NNNNNNNN        '`YMM@@@MMM@P'");
+		System.out.println("    MM@@M:'''' O:\":ONOO@MNOOOOXM@NM@NNN@P  -hrr-     \"`\"\"\"MM'");
+		System.out.println("    ''MM@:     \"' 'OOONMOYOOOOO@MM@MNNM\"");
+		System.out.println("      YM@'         :OOMN: :OOOO@MMNOXM'");
+		System.out.println("      `:P           :oP''  \"'OOM@NXNM'");
+		System.out.println("       `'                    ':OXNP'");
+		System.out.println("                               '\"'");
+
+		System.out.println("**************** Assignment 1.b - END ******************");	
+	}	
+}
